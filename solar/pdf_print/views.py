6f11a26da2433168e680fa5844f37fd9413a7acb
cknaut <can_knaut@yahoo.de>
2017-09-22 20:20:23 +0200

    Added Window View of Simulation Results

--- solar/pdf_print/views.py
+++ solar/pdf_print/views.py
@@ -2,28 +2,63 @@
 from __future__ import unicode_literals
 import numpy as np
 from django.shortcuts import render
+import pdb
 
 from django.http import HttpResponse
 
-from pdf_print.Solarclass import Solar
 
 
 def index(request):
-    
-    # Initial Positions for Planets
     names = np.array(["Sun", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"])
     ms = np.array([1.00000597682,  0.00095486104043, 0.000285583733151, 0.0000437273164546, 0.0000517759138449, 7.692307692307693e-09])
     qs = np.array([[0,  0,  0],  [-3.5023653,  -3.8169847,  -1.5507963], [9.0755314,  -3.0458353,  -1.6483708], [8.3101420,  -16.2901086,  -7.2521278], [11.4707666,  -25.7294829,  -10.8169456], [-15.5387357,  -25.2225594,  -3.1902382]])
     vs = np.array([[0, 0, 0],  [0.00565429,  -0.00412490,  -0.00190589], [0.00168318,  0.00483525,  0.00192462], [0.00354178,  0.00137102,  0.00055029], [0.00288930,  0.00114527,  0.00039677], [0.00276725,  -0.00170702,  -0.00136504]])
+    Tend = 20000
+    nsteps = 2000 
+    return print_pdf(request, names, ms, qs, vs, Tend, nsteps)
+    
 
+
+def print_pdf(request, names, ms, qs, vs, Tend, nsteps):
+    """performs evolution of gravitational problem with parameters
+
+    Input: 
+
+            names           ... np.array of planet names
+            ms              ... np.array of planet masses
+            qs              ... np.array of planet initial positions
+            vs              ... np.array of planet initial velocities
+            tEnd            ... np.array of planet names
+            nsteps          ... np.array of planet names
+
+    Output:
+            matplotlib-plot showing evolution prited on browser
+
+    """
+    from pdf_print.Solarclass import Solar
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    import django
+    
     # big bang
-    Solar_instance = Solar()
+    Solar = Solar()
 
     # add planets
-    Solar_instance.add_planets(names,  ms,  qs,  vs)
-    Solar_instance.evolve(20000,  2000)
-    path = Solar_instance.plot()
-    return HttpResponse("Simulation completed.")
+    Solar.add_planets(names,  ms,  qs,  vs)
+    Solar.evolve(Tend,  nsteps)    
+    qs = [planet.position for planet in Solar.planets]
+
+    fig=Figure(figsize=(12, 8))
+    ax=fig.add_subplot(111)
+    for i in range(Solar.n_planets):
+        q_planet = np.asarray(qs[i])
+        ax.plot(q_planet[:, 0], q_planet[:, 1], "-", label=Solar.planets[i].name)
+
+    canvas=FigureCanvas(fig)
+    response=django.http.HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    return response
+
 
 def pdf_view(request, path):
     with open(path, 'r') as pdf:
