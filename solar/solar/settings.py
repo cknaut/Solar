4881786d1c0d38dd8acf42216e10c4a2a11660e0
cknaut <can_knaut@yahoo.de>
2017-09-22 22:42:18 +0200

    Added STATIC_ROOT = none

--- solar/solar/settings.py
+++ solar/solar/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
+STATIC_ROOT = None  #  no static files for the moment
 STATIC_URL = '/static/'
 
 # Simplified static file serving.
