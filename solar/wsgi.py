872ac940de3dd8ee3db9adb835e29250da8e9201
cknaut <can_knaut@yahoo.de>
2017-09-22 23:34:56 +0200

    added reference to settings, corrected

--- solar/wsgi.py
+++ solar/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "solar.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
