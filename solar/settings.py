61d66cf5c0fae20768d3ec3b414bc5f88a2a0950
cknaut <can_knaut@yahoo.de>
2017-09-25 22:29:44 +0200

    Developpement Mode

--- solar/settings.py
+++ solar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%7d(@ggy-g@@1w8c5fg3%o+8qfml7g(7xp+q8h4+&8e_$j&+h_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = []
 
