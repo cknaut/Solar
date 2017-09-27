172559349e75ca9804058d791ff90e44423ad6c3
cknaut <can_knaut@yahoo.de>
2017-09-27 19:38:28 +0200

    Changed DEBUG and SECRET KEY to environment variables

--- solar/settings.py
+++ solar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%7d(@ggy-g@@1w8c5fg3%o+8qfml7g(7xp+q8h4+&8e_$j&+h_'
+SECRET_KEY = os.environ.get('DJANGO_SECRE_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = os.environ.get('DJANGO_DEBUG')
+
+
 
 ALLOWED_HOSTS = []
 
